228f961b0c8e62f125239017af1c26198663b508
akademuk <akademuk24@gmail.com>
2023-07-23 14:26:12 +0300

    sss

---
+++ script/industries-slider.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    var about = $('.industries-slider');
+
+    about.slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        infinite: false,
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                }
+            }
+        ]
+    });
+
+});
